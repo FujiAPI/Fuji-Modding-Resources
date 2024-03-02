73f270151c2bdba05c8cf6f0cb55063a3aa78f3f
Jasmine Stephens <jasminegamedev@gmail.com>
2024-03-02 12:26:15 -0800

    Setting up Merged FGD tool, and tools solution file. Also adding .gitignore

---
+++ Celeste64ModdingTools/MergeFGDTool/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+namespace MergeFGDTool
+{
+	public class Program
+	{
+		private static string fileName = "..\\Celeste64Merged.fgd";
+		private static string searchPath = AppContext.BaseDirectory + "..\\";
+
+		private const string fileNameKey = "filename=";
+		private const string searchPathKey = "searchpath=";
+		public static void Main(string[] args)
+		{
+			string? newFileName = args.FirstOrDefault(arg => arg.StartsWith(fileNameKey));
+			if(newFileName != null)
+			{
+				newFileName = newFileName.Substring(fileNameKey.Length);
+				if (!string.IsNullOrEmpty(newFileName))
+				{
+					if (newFileName.EndsWith(".fgd"))
+					{
+						fileName = newFileName;
+					}
+					else
+					{
+						fileName = newFileName + ".fgd";
+					}
+				}
+			}
+
+			string? newSearchPath = args.FirstOrDefault(arg => arg.StartsWith(searchPathKey));
+			if (newSearchPath != null)
+			{
+				newSearchPath = newSearchPath.Substring(searchPathKey.Length);
+				if (!string.IsNullOrEmpty(newSearchPath))
+				{
+					if(Directory.Exists(newSearchPath))
+					{
+						searchPath = newSearchPath;
+					}
+					else if( Directory.Exists(AppContext.BaseDirectory + newSearchPath))
+					{
+						searchPath = AppContext.BaseDirectory + newSearchPath;
+					}
+					else
+					{
+						Console.WriteLine($"Provided search path {searchPath} was not valid. Using default path instead");
+					}
+				}
+			}
+
+			string[] files = Directory.GetFiles(searchPath, "*.fgd", SearchOption.AllDirectories);
+
+			StringBuilder sb = new StringBuilder();
+
+			foreach (string file in files)
+			{
+				if (!file.Contains(fileName))
+				{
+					sb.AppendLine($"@include \"{Path.GetRelativePath(searchPath, file)}\"");
+				}
+			}
+
+			Console.WriteLine(sb.ToString());
+			File.WriteAllText(fileName, sb.ToString());
+		}
+	}
+}
